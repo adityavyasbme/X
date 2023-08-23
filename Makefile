91397e4e50d4f980791df900abce826d15408035
adityavyasbme <adityavyas1603@gmail.com>
2023-08-23 12:59:43 -0500

    Added a test streamlit page for testing

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+test-run-streamlit:
+	streamlit run frontend/tests/testStreamlitPage.py
---
+++ frontend/tests/testStreamlitPage.py
@@ -0,0 +1,40 @@
+# import module
+import streamlit as st
+ 
+# Title
+st.title("Hello Aditya !!!")
+
+# Header
+st.header("This is a header")
+ 
+# Subheader
+st.subheader("This is a subheader")
+
+# Text
+st.text("Hello Adi!!!")
+
+# Markdown
+st.markdown("### This is a markdown")
+
+# success
+st.success("Success")
+
+# success
+st.info("Information")
+
+# success
+st.warning("Warning")
+
+# success
+st.error("Error")
+
+# Exception - This has been added later
+exp = ZeroDivisionError("Trying to divide by Zero")
+st.exception(exp)
+
+# Write text
+st.write("Text with write")
+
+# Writing python inbuilt function range()
+st.write(range(10))
+
