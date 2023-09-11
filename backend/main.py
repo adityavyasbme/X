e7458ce1fcaf5a2d913122ac239e2d1873451179
adityavyasbme <adityavyas1603@gmail.com>
2023-09-11 03:25:25 -0700

    Disable OpenAPI docs; Using Redoc For Display

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,7 @@ app = FastAPI(
     title="Backend APIs For X",
     description="""This is just a test api""",
     version="0.1.0",
-    docs_url="/api/docs",
+    docs_url=None,
     openapi_url="/api/openapi.json"
 )
 
