a70be7d01a81b430f7fe7d58bae6247bd8360361
adityavyasbme <adityavyas1603@gmail.com>
2024-03-26 18:08:02 -0500

    updated project list

--- frontend/src/application/pages/page0_list.py
+++ frontend/src/application/pages/page0_list.py
@@ -18,5 +18,6 @@ st.write(f"""
     5. [Option Pricing using Black Scholes with Rule Based Recommendations ]({base_path}/Option%20Pricing%20using%20Black%20Scholes%20with%20BONUS%20Rule%20Based%20Recommendations)
     6. [Chat with PDF]({base_path}/Chat%20With%20PDF)
     7. [Youtube Summarizer ]({base_path}/Youtube%20Summarizer)
+    8. [Judger vs Learner Personality]({base_path}/Judger%20vs%20Learner%20Personality)
    
 """)
